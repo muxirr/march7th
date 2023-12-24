522750764316fca7259142c49ef28b421eee854f
EYJ <92171291+WuwuZiQWQ@users.noreply.github.com>
2023-12-25 00:51:27 +0800

    fix bug

--- bin/test.ps1
+++ bin/test.ps1
@@ -1,8 +1,6 @@
-<#
-.SYNOPSIS
-    Execute Pester tests in repository root directory.
-#>
+#Requires -Version 5.1
+#Requires -Modules @{ ModuleName = 'Pester'; ModuleVersion = '4.4.0' }
 
-$result = Invoke-Pester "$PSScriptRoot\.." -PassThru
-
+if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
+$result = Invoke-Pester "$PSScriptRoot\..\test" -PassThru
 exit $result.FailedCount
