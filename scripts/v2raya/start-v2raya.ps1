f9d87b6af066982e9556b8e5fb11af83df338b68
WuwuZiQWQ <mojong@qq.com>
2024-03-11 12:51:01 +0800

    111

--- scripts/v2raya/start-v2raya.ps1
+++ scripts/v2raya/start-v2raya.ps1
@@ -1,2 +1,2 @@
 $options = @("--lite", "--log-disable-timestamp", "--log-file v2raya.log", "--config $(scoop prefix v2raya)\config")
-Start-Process "$(scoop prefix v2raya)\v2rayaWin.exe" -ArgumentList $options -WorkingDirectory "$(scoop prefix v2raya)" -WindowStyle Hidden
+Start-Process "$(scoop prefix v2raya)\v2rayaWin.exe" -ArgumentList $options -WorkingDirectory "$(scoop prefix v2raya)\log" -WindowStyle Hidden
