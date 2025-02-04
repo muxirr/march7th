a3a10ab1dc657a62dff2c5519ef5c9a03b33fc63
muxirr <muxirr@qq.com>
2025-02-04 19:03:12 +0800

    fix liteloaderqqnt install bug, fix pixpin update bug

--- scripts/liteloaderqqnt/llqqnt_install.ps1
+++ scripts/liteloaderqqnt/llqqnt_install.ps1
@@ -1,4 +1,4 @@
-$qqnt_dir = "path/to/qqnt\Files"
+$qqnt_dir = "path/to/qqnt/Files"
 $llqqnt_dir = "path/to/llqqnt"
 $path = "$qqnt_dir\resources\app"
 
@@ -10,10 +10,10 @@ if((Test-Path $qqnt_dir\versions)) {
     $path = "$qqnt_dir\versions\$qq_version\resources\app"
 }
 
-Out-File -FilePath "$path\app_launcher\liteLoader.js" -InputObject $text -Encoding utf8 -Force
+Out-File -FilePath "$path\app_launcher\liteloader.js" -InputObject $text -Encoding utf8 -Force
 
 $package = Get-Content -Path $path\package.json | ConvertFrom-Json
 $package.main = "./app_launcher/liteloader.js"
 $package = $package | ConvertTo-Json
 $package = $package -replace "`r`n","`n"
-$package.TrimEnd("`r","`n") | Set-Content -Path $path\package.json -NoNewlin
+$package.TrimEnd("`r","`n") | Set-Content -Path $path\package.json -NoNewline
